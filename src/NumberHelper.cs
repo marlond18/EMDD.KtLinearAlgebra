7457a8ebeb748598d49206a4d8923b2abe6f3da6
marlond18 <marlon.dumalis@yahoo.com>
2021-03-21 23:48:19 +0800

    Initial Commit

---
+++ src/NumberHelper.cs
@@ -0,0 +1,58 @@
+﻿using KtExtensions;
+using System;
+
+namespace EMDD.KtNumerics
+{
+    public static class NumberHelper
+    {
+        public static double GetRealNumber(Number number)
+        {
+            return number.ToComplex().Real;
+        }
+
+        public static (Number min, Number max) MinMax(this (Number a, Number b) numbers) => (numbers.a < numbers.b) ? (numbers.a, numbers.b) : (numbers.b, numbers.a);
+
+        public static Number Sqrt(Number number)
+        {
+            return number.Sqrt();
+        }
+
+        /// <summary>
+        /// Greatest Common Dinaminator for two numbers
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static long Gcd(long left, long right)
+        {
+            if (left < 0) left = -left;
+            if (right < 0) right = -right;
+            if (left < 2 || right < 2) return 1;
+            do
+            {
+                if (left < right)
+                {
+                    var temp = left;
+                    left = right;
+                    right = temp;
+                }
+                left %= right;
+            } while (left != 0);
+            return right;
+        }
+
+        public static bool NumberIsWhole(Number val) => val is KtRealNumber real && (real.Value - Math.Truncate(real.Value)).NearZero();
+
+        public static Number Gcd1(Number left, Number right)
+        {
+            return (left, right) switch
+            {
+                (KtRealNumber realLeft, KtRealNumber realRight) when NumberIsWhole(realLeft) && NumberIsWhole(realRight) => Gcd((long)realLeft.Value, (long)realRight.Value),
+                _ => right
+            };
+        }
+
+        public static Number Gcd(Number left, Number right) =>
+            left is KtRealNumber realLeft && NumberIsWhole(realLeft) && right is KtRealNumber realRight && NumberIsWhole(realRight) ? Gcd((long)realLeft.Value, (long)realRight.Value) : right;
+    }
+}
---
+++ test/unit/KtComplexTest.cs
@@ -0,0 +1,24 @@
+﻿using EMDD.KtNumerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace KtNumericsTest
+{
+    [TestClass]
+    public class NumberTest
+    {
+        [TestMethod]
+        public void TostringTest()
+        {
+            Number b = (1, 2);
+            Assert.AreEqual(b.ToString(), "1+2i");
+            b = (1.0 / 3, 2);
+            Assert.AreEqual(b.ToString(), "0.333+2i");
+        }
+
+        [TestMethod]
+        public void EqualityTest()
+        {
+            Number n = 0;
+            Assert.IsTrue(n == 0);
+        }
+    }
+}
